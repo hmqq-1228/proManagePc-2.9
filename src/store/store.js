87cea785cf722662e0cd7e11455659b1963d3025
dongguang <642806067@qq.com>
2019-03-07 13:33:07 +0800

    2019-03-07 13:33

--- src/store/store.js
+++ src/store/store.js
@@ -21,11 +21,11 @@ const state = {
     {label: '产品研发', value: '5'}
   ],
   fileFormat: ['jpg', 'jpeg', 'png', 'gif'],
-  baseServiceUrl: 'http://10.0.5.241:8089'
+  // baseServiceUrl: 'http://10.0.5.241:8089'
   // baseServiceUrl: 'http://10.0.5.29:8088'
   // baseServiceUrl: 'https://ityp.baho.cn:8094/pms2'
   // baseServiceUrl: 'https://pms.baho.cn/pms'
-  // baseServiceUrl: 'https://ityp.baho.cn:8094/pms'
+  baseServiceUrl: 'https://ityp.baho.cn:8094/pms'
   // baseServiceUrl: 'http://10.0.5.241:8089'
 }
 const mutations = {
