bae496f376476ea34aa46b5ae0bd5699a4f12ef6
huangmeng <1665643628@qq.com>
2019-03-12 11:05:36 +0800

    0312-1105

--- src/components/ProEdit2.vue
+++ src/components/ProEdit2.vue
@@ -143,7 +143,7 @@
                 <span style="float: left;margin-left: 16px;">{{data.start}} - {{data.finish}}</span>
               </span>
               <span class="treeTime">
-                <Dropdown @on-click="moreSelectOptions($event, data.id, data.type)">
+                <Dropdown @on-click="moreSelectOptions($event, data.id, data.type, data)">
                   <a href="javascript:void(0)">操作<Icon type="ios-arrow-down"></Icon></a>
                   <DropdownMenu slot="list">
                     <DropdownItem name="add">添加</DropdownItem>
@@ -510,6 +510,58 @@
         </div>
       </Drawer>
       <!--新增 抽屉 编辑计划 end-->
+      <Drawer class="drawerScroll" title="修改任务" :closable="false" width="40%" v-model="modifyTaskVisible">
+        <div class="bgCoverCnt2" v-loading="loadingEdit">
+          <div class="colose" @click="modifyTaskVisible = false"><i class="el-icon-close"></i></div>
+          <div class="bgCoverTabs">
+            <div class="planTaskBox">
+              <el-form ref="modifyTask" :model="detailTaskform" :rules="modifyTaskRules" label-width="80px">
+                <el-form-item label="任务名称" prop="jobName" maxlength="100" width="100">
+                  <el-input class="planNameIpt" v-model="detailTaskform.jobName"></el-input>
+                </el-form-item>
+                <el-form-item label="任务级别" prop="jobLevel" maxlength="100" width="100">
+                  <div class="ratestar" style="padding-top: 6px;">
+                    <el-rate v-model="detailTaskform.jobLevel" v-on:change="levelChange($event)"></el-rate>
+                  </div>
+                </el-form-item>
+                <el-form-item label="开始时间" prop="taskStartDate">
+                  <el-col :span="24">
+                    <el-date-picker style="width: 100%" type="datetime"
+                                    format="yyyy-MM-dd HH:mm:ss"
+                                    value-format="yyyy-MM-dd HH:mm:ss"
+                                    placeholder="选择日期"
+                                    v-model="detailTaskform.taskStartDate"
+                                    :picker-options="pickerOptionsTaskSt"
+                    ></el-date-picker>
+                  </el-col>
+                </el-form-item>
+                <el-form-item label="结束时间" prop="taskFinishDate">
+                  <el-col :span="24">
+                    <el-date-picker type="datetime" style="width: 100%"
+                                    format="yyyy-MM-dd HH:mm:ss"
+                                    value-format="yyyy-MM-dd HH:mm:ss"
+                                    placeholder="选择日期"
+                                    v-model="detailTaskform.taskFinishDate"
+                                    :picker-options="pickerOptionsTaskSt"
+                    ></el-date-picker>
+                  </el-col>
+                </el-form-item>
+                <el-form-item label="任务描述" maxlength="100" width="100">
+                  <el-input class="planNameIpt" type="textarea" style="resize:none;" :rows="2" v-model="detailTaskform.description"></el-input>
+                </el-form-item>
+                <el-form-item label="任务附件">
+                  <component v-bind:is="FileUploadComp" fileFormId="TaskModify" v-bind:clearInfo="IsClear" v-on:FileDataEmit="GetFileInfo"></component>
+                </el-form-item>
+                <div style="text-align: center">
+                  <el-button type="primary" @click="modifyTaskSub('modifyTask')">保存</el-button>
+                  <el-button @click="modifyTaskVisible = false">关 闭</el-button>
+                </div>
+              </el-form>
+            </div>
+            <!---->
+          </div>
+        </div>
+      </Drawer>
       <!--新增 抽屉 任务详情 start-->
       <Drawer class="drawerScroll" :closable="false" width="750" v-model="value4">
         <div class="slidTop">
@@ -921,6 +973,32 @@ export default {
       pickerOptionsPlanEt: {},
       pickerOptionsTaskSt: {},
       pickerOptionsTaskEt: {},
+      // 新建 修改任务
+      detailTaskform: {
+        id: '',
+        jobName: '',
+        jobLevel: 2,
+        taskStartDate: '2018-10-10 00:00:00',
+        taskFinishDate: '2018-10-10 00:00:00',
+        description: ''
+      },
+      modifyTaskVisible: false,
+      loadingEdit: false,
+      // 新建 修改任务
+      modifyTaskRules: {
+        jobName: [
+          { required: true, message: '请输入任务名称', trigger: 'blur' }
+        ],
+        jobLevel: [
+          { required: true, message: '请选择任务等级', trigger: 'change' }
+        ],
+        taskStartDate: [
+          { type: 'string', required: true, message: '请选择开始日期', trigger: 'change' }
+        ],
+        taskFinishDate: [
+          { type: 'string', required: true, message: '请选择结束日期', trigger: 'change' }
+        ]
+      },
       // 编辑计划
       planRules: {
         name: [
@@ -1697,7 +1775,7 @@ export default {
       })
     },
     // 节点操作 展开更多 添加
-    moreSelectOptions: function (nodeName, nodeId, nodeType) {
+    moreSelectOptions: function (nodeName, nodeId, nodeType, nodeData) {
       this.log('nodeName:', nodeName)
       this.log('nodeId:', nodeId)
       this.currentNodeId = nodeId
@@ -1706,7 +1784,34 @@ export default {
       } else if (nodeName === 'del') {
         this.modal2 = true
         // this.delNode(nodeId)
+      } else if (nodeName === 'edit') {
+        if (nodeType === '2') {
+          this.modifyTask(nodeId)
+        } else if (nodeType === '1') {
+          this.detailform.name = nodeData.name
+          this.detailform.date1 = nodeData.start
+          this.detailform.date2 = nodeData.finish
+          this.detailform.description = nodeData.description
+          this.planEditShow = true
+        }
       }
+    },
+    // 编辑任务 修改任务
+    modifyTask: function (id) {
+      var that = this
+      that.taskIdEdit = id
+      that.modifyTaskVisible = true
+      that.ajax('/myTask/queryTaskDetailSingle', {taskId: id}).then(res => {
+        that.detailTaskform.jobName = res.data.jobName
+        that.detailTaskform.jobLevel = parseInt(res.data.jobLevel)
+        that.detailTaskform.taskStartDate = res.data.taskStartDate
+        that.detailTaskform.taskFinishDate = res.data.taskFinishDate
+        that.detailTaskform.description = res.data.description
+        for (var i = 0; i < res.data.attachment.length; i++) {
+          res.data.attachment[i].attachmentId = res.data.attachment[i].id
+        }
+        that.fileListEdit = res.data.attachment
+      })
     },
     // 新建 添加子节点
     addNode: function (nodeId, nodeType) {
@@ -3148,6 +3253,49 @@ export default {
       }
     },
     // 编辑计划 保存
+    levelChange: function (rateval) {
+      this.detailTaskform.jobLevel = rateval
+    },
+    modifyTaskSub: function (formName) {
+      var that = this
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          var fileStr = ''
+          for (var j = 0; j < this.fileListEdit.length; j++) {
+            if (j === that.fileListEdit.length - 1) {
+              fileStr = fileStr + that.fileListEdit[j].attachmentId
+            } else {
+              fileStr = fileStr + that.fileListEdit[j].attachmentId + ','
+            }
+          }
+          that.loadingEdit = true
+          that.editTaskPayload.id = that.taskIdEdit
+          that.editTaskPayload.jobLevel = that.detailTaskform.jobLevel
+          that.editTaskPayload.jobName = that.detailTaskform.jobName
+          that.editTaskPayload.taskStartDate = that.detailTaskform.taskStartDate
+          that.editTaskPayload.taskFinishDate = that.detailTaskform.taskFinishDate
+          that.editTaskPayload.description = that.detailTaskform.description
+          that.editTaskPayload.attachmentId = that.SetFileIdStr()
+          that.ajax('/myProject/editTask', that.editTaskPayload).then(res => {
+            that.log('editTask:', res)
+            if (res.code === 200) {
+              that.$message({
+                message: '保存成功！',
+                type: 'success'
+              })
+              that.IsClear = true
+              that.modifyTaskVisible = false
+              that.loadingEdit = false
+              that.toDetail(that.taskIdEdit)
+              that.selectProjectId()
+              that.getHistoryList()
+            } else {
+              that.loadingEdit = false
+            }
+          })
+        }
+      })
+    },
     // 修改点击了保存
     modifySub: function (formName) {
       // this.alert('计划点击了保存')
