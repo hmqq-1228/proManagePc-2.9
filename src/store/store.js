d2c4aab206b0b7a0761f936df03227e9a1aeeac2
zhanghe <1043123612@qq.com>
2019-06-13 09:48:15 +0800

    2019/6/11

--- src/store/store.js
+++ src/store/store.js
@@ -78,13 +78,9 @@ const state = {
   // baseServiceUrl: 'https://ityp.baho.cn:8094/pms2'
   // baseServiceUrl: 'https://pms.baho.cn/pms'
   baseServiceUrl: 'https://ityp.baho.cn:8094/pms'
-<<<<<<< HEAD
-  // baseServiceUrl: 'http://10.0.5.29:8088'
-=======
   // 郭宋恒
   // baseServiceUrl: 'http://10.0.5.241:8089'
   // baseServiceUrl: 'http://10.0.5.240:8088'
->>>>>>> dev
 }
 const mutations = {
   add (state) {
